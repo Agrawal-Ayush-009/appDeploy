19b2edbeaea672fd9346476decb3be46bb3af766
Agrawal-Ayush-009 <ayushmaserati@gmail.com>
2024-05-01 01:03:30 +0530

    index.js update

--- index.js
+++ index.js
@@ -3,6 +3,44 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
+const githubData = {
+  login: "Agrawal-Ayush-009",
+  id: 118624479,
+  node_id: "U_kgDOBxIQ3w",
+  avatar_url: "https://avatars.githubusercontent.com/u/118624479?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/Agrawal-Ayush-009",
+  html_url: "https://github.com/Agrawal-Ayush-009",
+  followers_url: "https://api.github.com/users/Agrawal-Ayush-009/followers",
+  following_url:
+    "https://api.github.com/users/Agrawal-Ayush-009/following{/other_user}",
+  gists_url: "https://api.github.com/users/Agrawal-Ayush-009/gists{/gist_id}",
+  starred_url:
+    "https://api.github.com/users/Agrawal-Ayush-009/starred{/owner}{/repo}",
+  subscriptions_url:
+    "https://api.github.com/users/Agrawal-Ayush-009/subscriptions",
+  organizations_url: "https://api.github.com/users/Agrawal-Ayush-009/orgs",
+  repos_url: "https://api.github.com/users/Agrawal-Ayush-009/repos",
+  events_url: "https://api.github.com/users/Agrawal-Ayush-009/events{/privacy}",
+  received_events_url:
+    "https://api.github.com/users/Agrawal-Ayush-009/received_events",
+  type: "User",
+  site_admin: false,
+  name: "Ayush Agrawal",
+  company: null,
+  blog: "",
+  location: "Noida, Uttar Pradesh",
+  email: null,
+  hireable: null,
+  bio: null,
+  twitter_username: null,
+  public_repos: 19,
+  public_gists: 0,
+  followers: 3,
+  following: 2,
+  created_at: "2022-11-19T16:37:21Z",
+  updated_at: "2024-04-30T14:11:38Z",
+};
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -14,6 +52,10 @@ app.get("/twitter", (req, res) => {
 
 app.get("/ayush", (req, res) => {
   res.send("<h1> This is Ayush Agrawal's Route </h1>");
+});
+
+app.get("/github", (req, res) => {
+  res.json(githubData);
 });
 
 app.listen(port, () => {
